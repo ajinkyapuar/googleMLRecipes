99e4575fc981a2a0415c7cb01b2dd2676fefe51d
Ajinkya <albus702@gmail.com>
2017-05-12 17:30:00 +0530

    decision tree classifier accuracy

--- decisionTreeClassifier.py
+++ decisionTreeClassifier.py
@@ -44,62 +44,62 @@
 
 ############################################################################################################################################
 
-import numpy as np
-from sklearn.datasets import load_iris
-from sklearn import tree
-
-from sklearn.externals.six import StringIO
-import pydot
-
-iris = load_iris()
-
-test_idx = [0, 50, 100]
-
-# print "********************** BEGIN IRIS DATA **********************"
-# print iris.data
-# print "********************** END IRIS DATA **********************"
+# import numpy as np
+# from sklearn.datasets import load_iris
+# from sklearn import tree
 #
-# print "********************** BEGIN IRIS TARGET **********************"
-# print iris.target
-# print "********************** END IRIS TARGET **********************"
-
-# Training Data
-train_data = np.delete(iris.data, test_idx, axis=0)
-train_target = np.delete(iris.target, test_idx, axis=0)
-
-# print "********************** BEGIN TRAIN DATA **********************"
+# from sklearn.externals.six import StringIO
+# import pydot
 #
-# print train_data
+# iris = load_iris()
 #
-# print "********************** END TRAIN DATA **********************"
+# test_idx = [0, 50, 100]
 #
-# print "********************** BEGIN TRAIN TARGET **********************"
+# # print "********************** BEGIN IRIS DATA **********************"
+# # print iris.data
+# # print "********************** END IRIS DATA **********************"
+# #
+# # print "********************** BEGIN IRIS TARGET **********************"
+# # print iris.target
+# # print "********************** END IRIS TARGET **********************"
 #
-# print train_target
+# # Training Data
+# train_data = np.delete(iris.data, test_idx, axis=0)
+# train_target = np.delete(iris.target, test_idx, axis=0)
 #
-# print "********************** END TRAIN TARGET **********************"
-
-# Testing Data
-test_data = iris.data[test_idx]
-test_target = iris.target[test_idx]
-
-# print test_data
+# # print "********************** BEGIN TRAIN DATA **********************"
+# #
+# # print train_data
+# #
+# # print "********************** END TRAIN DATA **********************"
+# #
+# # print "********************** BEGIN TRAIN TARGET **********************"
+# #
+# # print train_target
+# #
+# # print "********************** END TRAIN TARGET **********************"
+#
+# # Testing Data
+# test_data = iris.data[test_idx]
+# test_target = iris.target[test_idx]
+#
+# # print test_data
+# # print test_target
+# #
+# clf = tree.DecisionTreeClassifier()
+#
+# clf.fit(train_data, train_target)
+#
 # print test_target
+# print clf.predict(test_data)
 #
-clf = tree.DecisionTreeClassifier()
-
-clf.fit(train_data, train_target)
-
-print test_target
-print clf.predict(test_data)
-
-dot_data = StringIO()
+# dot_data = StringIO()
 # print dot_data
-tree.export_graphviz(clf, out_file=dot_data, feature_names=iris.feature_names, class_names=iris.target_names,
-                     filled=True, rounded=True, impurity=False)
-
-graph = pydot.graph_from_dot_data(dot_data.getvalue())
-graph.write_pdf("iris.pdf")
+# tree.export_graphviz(clf, out_file=dot_data, feature_names=iris.feature_names, class_names=iris.target_names,
+#                      filled=True, rounded=True, impurity=False)
+#
+# graph = pydot.graph_from_dot_data(dot_data.getvalue())
+# graph.write_pdf("iris.pdf")
 
 # open -a preview iris.pdf
 
@@ -195,40 +195,40 @@ graph.write_pdf("iris.pdf")
 
 ############################################################################################################################################
 
-# from sklearn import datasets
-# from sklearn.cross_validation import train_test_split
-# from sklearn import tree
-# from sklearn.metrics import accuracy_score
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5)
+
+
+# print "********************** BEGIN TRAIN DATA **********************"
 #
-# iris = datasets.load_iris()
+# print X_train
 #
-# X = iris.data
-# y = iris.target
+# print "********************** END TRAIN DATA **********************"
 #
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5)
+# print "********************** BEGIN TRAIN TARGET **********************"
 #
+# print y_train
 #
-# # print "********************** BEGIN TRAIN DATA **********************"
-# #
-# # print X_train
-# #
-# # print "********************** END TRAIN DATA **********************"
-# #
-# # print "********************** BEGIN TRAIN TARGET **********************"
-# #
-# # print y_train
-# #
-# # print "********************** END TRAIN TARGET **********************"
-#
-# clf = tree.DecisionTreeClassifier()
-#
-# clf.fit(X_train, y_train)
-#
-# predictions = clf.predict(X_test)
-#
-# print predictions
-#
-# print accuracy_score(y_test, predictions)
+# print "********************** END TRAIN TARGET **********************"
+
+clf = tree.DecisionTreeClassifier()
+
+clf.fit(X_train, y_train)
+
+predictions = clf.predict(X_test)
+
+print predictions
+
+print accuracy_score(y_test, predictions)
 
 
 ############################################################################################################################################
