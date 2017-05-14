3ea83c9e03ad0d22138855a7e7826b225667edfd
Ajinkya <albus702@gmail.com>
2017-05-15 01:27:40 +0530

    links and concepts

--- imageClassifier.py
+++ imageClassifier.py
@@ -5,6 +5,14 @@
 
 ############################################################################################################################################
 
+# https://codelabs.developers.google.com/codelabs/tensorflow-for-poets/?utm_campaign=chrome_series_machinelearning_063016&utm_source=gdev&utm_medium=yt-desc#0
+
+# https://github.com/random-forests/tutorials/blob/master/ep7.ipynb
+
 ############################################################################################################################################
+
+# inception : one of googles best image classifiers
+
+# we will be using inception and retraining it (transfer learning)
 
 ############################################################################################################################################
